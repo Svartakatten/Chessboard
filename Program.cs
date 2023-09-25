ddb7b26f54f16a4226ebe8f4c4cddeb3cfd15432
Svartakatten <rikexhx@gmail.com>
2023-09-25 17:23:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Chessboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Vänligen skriv i en siffra");
+
+            int amount = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < amount; i++)
+            {
+                for (int j = 0; j < amount; j++)
+                {
+                    if ((j + i) % 2 == 0)
+                    {
+                        Console.Write("□");
+                        Console.Write("\t");
+                    }
+                    else
+                    {
+                        Console.Write("■");
+                        Console.Write("\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
